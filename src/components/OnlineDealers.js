da41e8c9d8278777880931388a7b9a12abfb9a7e
Nathanael Smith <nathanaelnsmith@gmail.com>
2021-11-17 12:24:18 -0800

    online dealers page

---
+++ src/components/OnlineDealers.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+
+export class OnlineDealers extends Component {
+  state = {
+    dealers: [
+      {
+        title: 'Backcountry',
+        url: 'https://www.backcountry.com/',
+        logo: 'Backcountry-150x94.jpg'
+      },
+      {
+        title: 'REI',
+        url: 'https://www.rei.com/',
+        logo: 'REI-150x94.jpg'
+      },
+      {
+        title: 'EVO',
+        url: 'https://www.evo.com/',
+        logo: 'EVO-150x94.jpg'
+      },
+      {
+        title: 'Next Adventure',
+        url: 'https://www.nextadventure.net/',
+        logo: 'NextAdventure-150x94.jpg'
+      },
+      {
+        title: 'The Source',
+        url: 'https://www.sourceboards.com/',
+        logo: 'TheSource-150x94.jpg'
+      },
+      {
+        title: 'The Boardroom',
+        url: 'https://boardroomshop.com/store/',
+        logo: 'TheBoardroom-150x94.jpg'
+      },
+      {
+        title: 'Sanction',
+        url: 'https://sanctionsnow.com/',
+        logo: 'Sanction-150x94.jpg'
+      },
+      {
+        title: 'Nomads',
+        url: 'https://shopnomads.com/',
+        logo: 'Nomads-150x94.jpg'
+      },
+      {
+        title: 'Moosejaw',
+        url: 'https://www.moosejaw.com/',
+        logo: 'Moosejaw-150x94.jpg'
+      },
+      {
+        title: 'Milosport',
+        url: 'https://www.milosport.com/',
+        logo: 'Milosport-150x94.jpg'
+      },
+      {
+        title: 'Baker Street',
+        url: 'https://bakerstsnow.com',
+        logo: 'BakerStreet-150x94.jpg'
+      },
+      {
+        title: 'Blauer Board Shop',
+        url: 'https://www.blauerboardshop.com/',
+        logo: 'Blauers-150x94.png'
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="online-dealers">
+        <h1>Online Dealers</h1>
+        <p>Where can you buy a Niche snowboard?  Through your local shop, from one of websites below, or directly from our website.</p>
+        <div className="grid">
+        {this.state.dealers.map((dealer, i) =>
+          <div key={i} className="dealer">
+            <a href={dealer.url} target="niche_ext"><img src={`/dealers/dealer-logos/${dealer.logo}`} alt={dealer.title} /></a>
+          </div>
+         )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default OnlineDealers
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Posts from './components/Posts';
 import Dealers from './components/Dealers';
+import OnlineDealers from './components/OnlineDealers';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -21,6 +22,7 @@ ReactDOM.render(
         <Routes>
           <Route path="/posts" element={<Posts />} />
           <Route path="/dealers" element={<Dealers />} />
+          <Route path="/online-dealers" element={<OnlineDealers />} />
           <Route path="/" element={<Posts />} />
         </Routes>
       </div>
